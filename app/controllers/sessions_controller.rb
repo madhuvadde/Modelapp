97b5cea76a0703cc75f9a1287ff6e83e90b1d120
madhuvadde <madhuvadde237@gmail.com>
2017-12-28 22:58:28 +0530

    make it better

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
 	  
+
+
          def create
 		user=User.find_by(email: params[:email])
 		if user && user.authenticate(params[:password])
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
  
+  get 'developers/index'
+
     root "home#index"
     resources :home
-	#root "users#new"
 	resources :users
 	resources :sessions
 	resources :posts
  #get "/users/new" => "users#new"
- 
+  
 end
